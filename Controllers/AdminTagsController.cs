26068eb486c4d4dce8e0e421424724395a314000
samuelasiabaka <samuelasiabaka@gmail.com>
2025-03-23 16:10:27 +0100

    Add project files.

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBlog.Data;
+using WebBlog.Models.Domain;
+using WebBlog.Models.ViewModels;
+
+namespace WebBlog.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly WebBlogDbContext webBlogDbContext;
+
+        public AdminTagsController(WebBlogDbContext webBlogDbContext)
+        {
+            this.webBlogDbContext = webBlogDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            //Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            webBlogDbContext.Tags.Add(tag);
+            webBlogDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ Data/WebBlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebBlog.Models.Domain;
+
+namespace WebBlog.Data
+{
+    public class WebBlogDbContext: DbContext
+    {
+        public WebBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
